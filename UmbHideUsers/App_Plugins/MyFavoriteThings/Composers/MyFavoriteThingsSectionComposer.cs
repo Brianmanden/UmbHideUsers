4c016aa802006c3f83464aa77a18d096508952af
Brianmanden <brianmanden@gmail.com>
2020-11-21 15:58:53 +0100

    Init Umbraco + Add empty Custom User List section to dashboard.

---
+++ UmbHideUsers/App_Plugins/MyFavoriteThings/Composers/MyFavoriteThingsSectionComposer.cs
@@ -0,0 +1,13 @@
+﻿using UmbHideUsers.App_Plugins.MyFavoriteThings.Sections;
+using Umbraco.Core.Composing;
+using Umbraco.Web;
+
+namespace UmbHideUsers.App_Plugins.MyFavoriteThings
+{
+    public class SectionComposer : IUserComposer
+    {
+        public void Compose(Composition composition) {
+            composition.Sections().Append<MyFavoriteThingsSection>();
+        }
+    }
+}
---
+++ UmbHideUsers/App_Plugins/MyFavoriteThings/Sections/MyFavoriteThingsSection.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Core.Models.Sections;
+
+namespace UmbHideUsers.App_Plugins.MyFavoriteThings.Sections
+{
+    public class MyFavoriteThingsSection : ISection
+    {
+        public string Alias => "myFavoriteThings";
+
+        public string Name => "My Favorite Things";
+    }
+}
