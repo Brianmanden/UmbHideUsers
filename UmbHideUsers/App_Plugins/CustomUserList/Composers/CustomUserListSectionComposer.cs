329da7df9fc214b13ee98b35ad29f2ced9165247
Brianmanden <brianmanden@gmail.com>
2020-11-21 16:00:27 +0100

    Add rest of files.

---
+++ UmbHideUsers/App_Plugins/CustomUserList/Composers/CustomUserListSectionComposer.cs
@@ -0,0 +1,14 @@
+﻿using UmbHideUsers.App_Plugins.CustomUserList.Sections;
+using Umbraco.Core.Composing;
+using Umbraco.Web;
+
+namespace UmbHideUsers.App_Plugins.CustomUserList.Composers
+{
+    public class SectionComposer : IUserComposer
+    {
+        public void Compose(Composition composition) {
+            composition.Sections().Append<CustomUserListSection>();
+            composition.Sections().Insert<CustomUserListSection>(4);
+        }
+    }
+}
---
+++ UmbHideUsers/App_Plugins/CustomUserList/Sections/CustomUserListSection.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Core.Models.Sections;
+
+namespace UmbHideUsers.App_Plugins.CustomUserList.Sections
+{
+    public class CustomUserListSection : ISection
+    {
+        public string Alias => "customUserList";
+
+        public string Name => "Custom User List";
+    }
+}
